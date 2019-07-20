04f659d0796c14495debe0d8a6247896b1c0a220
Dylan Socolobsky <dsocolobsky@gmail.com>
2019-07-20 10:13:45 -0300

    Makes it work again

--- trivialbot.py
+++ trivialbot.py
@@ -8,6 +8,10 @@ import redis
 
 API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
 TELEGRAM_ADMIN_ID = os.environ.get('TELEGRAM_ADMIN_ID')
+CORRECT_ACC = 0.85
+CLOSE_ACC = 0.75
+
+current_question = None
 
 def replace_str_index(text,index,replacement):
     return '%s%s%s'%(text[:index],replacement,text[index+1:])
@@ -35,28 +39,19 @@ class Question():
         self.category = q['category']['title']
         self.length = len(self.mild_answer)
         self.masked = re.sub("[^ ]", "*", self.mild_answer)
-        self.pista_idx = self.length
+        self.pista_idx = 0
 
     def pista(self):
-        if self.pista_idx >= 0:
-            ri = random.randint(0, self.length-1)
-            i = 0
-            while self.masked[ri] != '*':
-                ri = random.randint(0, self.length-1)
-                i += 1
-                if i > self.length:
-                    break
+        if self.pista_idx >= self.length:
+            return None
 
-            print(f'ri: {ri}')
-            if self.masked[ri] != '*':
-                print('Nope')
-                return None
-            else:
-                self.masked = replace_str_index(self.masked, ri, self.mild_answer[ri])
-                self.pista_idx -= 1
-                print('New Masked: {self.masked}')
-                return self.masked
-        return None
+        ri = random.randint(0, self.length-1)
+        while self.masked[ri] != '*':
+            ri = random.randint(0, self.length-1)
+
+        self.masked = replace_str_index(self.masked, ri, self.mild_answer[ri])
+        self.pista_idx += 1
+        return self.masked
 
     def check(self, answ):
         return seqmatch(a=sanitize_full(answ), b=self.san_answer).ratio()
@@ -66,7 +61,6 @@ class Question():
 
     def __str__(self):
         return f'{self.category} ({self.length} caracteres)\n==========================\n{self.text}'
-
 
 def get_question(update, context):
     global current_question
@@ -93,16 +87,20 @@ def handle_msg(update, context):
     txt = update.message.text
     if (txt == '!ranking'):
         print_ranking(update, context)
-    elif (txt == '!dame' and update.message.from_user.id == TELEGRAM_ADMIN_ID):
+    elif (txt == '!dame'): #and update.message.from_user.id == TELEGRAM_ADMIN_ID):
+        if current_question is not None:
+            msg = f'La respuesta era {current_question.mild_answer}'
+            context.bot.send_message(chat_id=update.message.chat_id, text=msg)
+        
         get_question(update, context)
     elif (txt == '!pista' and current_question):
         pista = current_question.pista()
-        if pista != None:
+        if pista:
             context.bot.send_message(chat_id=update.message.chat_id, text=pista)
     elif current_question:
         ratio = current_question.check(txt)
         print(f'El ratio es {ratio}')
-        if (ratio > 0.85):
+        if (ratio > CORRECT_ACC):
             usr = update.message.from_user
             usrid = str(usr.id)
             n = db.zadd('ranking', {usrid: '1'}, incr=True)
@@ -116,30 +114,27 @@ def handle_msg(update, context):
             else:
                 name = db.get(usrid).decode('utf-8')
                 
-            answ = current_question.mild_answer
-            msg = f'Correcto {name}! La respuesta era {answ}'
+            msg = f'Correcto {name}! La respuesta era {current_question.mild_answer}'
             context.bot.send_message(chat_id=update.message.chat_id, text=msg)
-
             print_ranking(update, context)
             get_question(update, context)
-        elif ratio > 0.75:
+        elif ratio > CLOSE_ACC:
             context.bot.send_message(chat_id=update.message.chat_id, text="Alguien esta muy cerca!")
 
 def main():
     global db
     db = redis.StrictRedis(host="localhost", port=6379, db=0, socket_keepalive=True, socket_timeout=300)
-    print(f'db: {db}')
-    
+    #if not db.connection_pool._available_connections:
+    #    sys.exit("Failed to connect to Redis. Are you sure it's running?")
+
     updater = Updater(API_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text, handle_msg))
     
     print('Bot initialized')
-    
     updater.start_polling()
     updater.idle()
     
-
 if __name__ == '__main__':
     main()
 
