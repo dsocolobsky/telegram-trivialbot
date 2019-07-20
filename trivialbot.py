8ea2c745f2284abf7a9eb8d0c72821330a904c0a
Martín del Río <11744462+Lartu@users.noreply.github.com>
2019-07-20 01:37:34 -0300

    Translated the codebase into English

--- trivialbot.py
+++ trivialbot.py
@@ -35,10 +35,10 @@ class Question():
         self.category = q['category']['title']
         self.length = len(self.mild_answer)
         self.masked = re.sub("[^ ]", "*", self.mild_answer)
-        self.pista_idx = self.length
+        self.clue_idx = self.length
 
-    def pista(self):
-        if self.pista_idx >= 0:
+    def clue(self):
+        if self.clue_idx >= 0:
             ri = random.randint(0, self.length-1)
             i = 0
             while self.masked[ri] != '*':
@@ -53,7 +53,7 @@ class Question():
                 return None
             else:
                 self.masked = replace_str_index(self.masked, ri, self.mild_answer[ri])
-                self.pista_idx -= 1
+                self.clue_idx -= 1
                 print('New Masked: {self.masked}')
                 return self.masked
         return None
@@ -65,7 +65,7 @@ class Question():
         return f'Answer: {self.answer} | {self.mild_answer} | {self.san_answer}'
 
     def __str__(self):
-        return f'{self.category} ({self.length} caracteres)\n==========================\n{self.text}'
+        return f'{self.category} ({self.length} characters)\n==========================\n{self.text}'
 
 
 def get_question(update, context):
@@ -79,7 +79,7 @@ def get_question(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=str(current_question))
     
 def print_ranking(update, context):
-    msg = 'Ranking Actual\n===========\n'
+    msg = 'Current Ranking\n===========\n'
     ranking = db.zrevrange('ranking', 0, -1, withscores=True)
     for rank in ranking:
         name = db.get(rank[0]).decode('utf-8')
@@ -93,20 +93,20 @@ def handle_msg(update, context):
     txt = update.message.text
     if (txt == '!ranking'):
         print_ranking(update, context)
-    elif (txt == '!dame' and update.message.from_user.id == TELEGRAM_ADMIN_ID):
+    elif (txt == '!ask' and update.message.from_user.id == TELEGRAM_ADMIN_ID):
         get_question(update, context)
-    elif (txt == '!pista' and current_question):
-        pista = current_question.pista()
-        if pista != None:
-            context.bot.send_message(chat_id=update.message.chat_id, text=pista)
+    elif (txt == '!clue' and current_question):
+        clue = current_question.clue()
+        if clue != None:
+            context.bot.send_message(chat_id=update.message.chat_id, text=clue)
     elif current_question:
         ratio = current_question.check(txt)
-        print(f'El ratio es {ratio}')
+        print(f'The current ratio is {ratio}')
         if (ratio > 0.85):
             usr = update.message.from_user
             usrid = str(usr.id)
             n = db.zadd('ranking', {usrid: '1'}, incr=True)
-            # Nuestro usuario no existia en la DB
+            # Our user didn't exist in the database
             if n == 1.0:
                 if usr.last_name != None:
                     name = usr.first_name + usr.last_name
@@ -117,13 +117,13 @@ def handle_msg(update, context):
                 name = db.get(usrid).decode('utf-8')
                 
             answ = current_question.mild_answer
-            msg = f'Correcto {name}! La respuesta era {answ}'
+            msg = f'Right, {name}! The answer was {answ}'
             context.bot.send_message(chat_id=update.message.chat_id, text=msg)
 
             print_ranking(update, context)
             get_question(update, context)
         elif ratio > 0.75:
-            context.bot.send_message(chat_id=update.message.chat_id, text="Alguien esta muy cerca!")
+            context.bot.send_message(chat_id=update.message.chat_id, text="Someone's very close!")
 
 def main():
     global db
